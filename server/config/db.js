e105a9f8d06ffb1caa4dbd2bf3278a044251378e
BasstianJacome <vjacomeg@my.centennialcollege.ca>
2023-03-02 16:51:13 -0500

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+/*
+    Page: db.js
+    Name: Vinicio Jacome
+    Student Id: 301289381
+    Date: 03/02/2023
+*/
+
+
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://viniciojacomeg:TiwGSnL3TrTx1yJB@cluster0.zv93hhk.mongodb.net/book_store?retryWrites=true&w=majority"
+};
